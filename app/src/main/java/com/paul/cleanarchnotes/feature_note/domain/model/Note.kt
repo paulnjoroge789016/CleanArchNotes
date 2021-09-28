142dbf22da49d926b0328d2306ed01bbed98d435
Tekkiller <paulnjoroge789016@gmail.com>
2021-09-28 18:04:32 +0300

    Implemented Dao for our Notes.

---
+++ app/src/main/java/com/paul/cleanarchnotes/feature_note/domain/model/Note.kt
@@ -0,0 +1,16 @@
+package com.paul.cleanarchnotes.feature_note.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.paul.cleanarchnotes.ui.theme.*
+
+@Entity
+data class Note(
+    val title: String,
+    val content: String,
+    val timestamp: Long,
+    val color: Int
+){
+    @PrimaryKey val id: Int = 0
+    val noteColors= listOf( RedOrange, LightGreen, Violet, BabyBlue, RedPink)
+}
