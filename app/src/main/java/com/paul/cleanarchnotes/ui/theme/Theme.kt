15d73039040c88050d76650017112c05ef0cbb11
Tekkiller <paulnjoroge789016@gmail.com>
2021-09-28 17:41:11 +0300

    Initial commit

---
+++ app/src/main/java/com/paul/cleanarchnotes/ui/theme/Theme.kt
@@ -0,0 +1,25 @@
+package com.paul.cleanarchnotes.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors(
+    primary = Color.White,
+    background = DarkGray,
+    onBackground = Color.White,
+    surface = LightBlue,
+    onSurface = DarkGray
+)
+
+@Composable
+fun CleanArchNotesTheme(darkTheme: Boolean = true, content: @Composable() () -> Unit) {
+    MaterialTheme(
+        colors = DarkColorPalette,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
