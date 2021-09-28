3effbefeeee7e07e321831742ac7f3ad490d680b
Tekkiller <paulnjoroge789016@gmail.com>
2021-09-28 18:11:57 +0300

    Note Repository domain level.

--- app/src/main/java/com/paul/cleanarchnotes/feature_note/domain/repository/NoteRepository.kt
+++ app/src/main/java/com/paul/cleanarchnotes/feature_note/domain/repository/NoteRepository.kt
@@ -1,4 +1,18 @@
 package com.paul.cleanarchnotes.feature_note.domain.repository
 
+import com.paul.cleanarchnotes.feature_note.domain.model.Note
+import kotlinx.coroutines.flow.Flow
+
 interface NoteRepository {
+
+    fun getNotes(): Flow<List<Note>>
+
+    suspend fun getNoteById(id : Int): Note?
+
+    suspend fun insertNote(note: Note)
+
+    suspend fun deleteNote(note: Note)
+
+
+
 }
