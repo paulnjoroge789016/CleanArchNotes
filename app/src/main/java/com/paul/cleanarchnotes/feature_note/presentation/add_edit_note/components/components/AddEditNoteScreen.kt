9ae68dcfe5f868bc34cde5e57c092084c6edca93
Tekkiller <paulnjoroge789016@gmail.com>
2021-10-11 11:01:22 +0300

    Adding add edit note screen

--- app/src/main/java/com/paul/cleanarchnotes/feature_note/presentation/add_edit_note/components/components/AddEditNoteScreen.kt
+++ app/src/main/java/com/paul/cleanarchnotes/feature_note/presentation/add_edit_note/components/components/AddEditNoteScreen.kt
@@ -1,15 +1,32 @@
 package com.paul.cleanarchnotes.feature_note.presentation.add_edit_note.components.components
 
 import androidx.compose.animation.Animatable
-import androidx.compose.material.rememberScaffoldState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Save
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
+import com.paul.cleanarchnotes.feature_note.domain.model.Note
+import com.paul.cleanarchnotes.feature_note.presentation.add_edit_note.components.AddEditNoteEvent
 import com.paul.cleanarchnotes.feature_note.presentation.add_edit_note.components.AddEditNoteViewModel
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @Composable
 fun AddEditNoteScreen(
@@ -24,8 +41,126 @@ fun AddEditNoteScreen(
 
     val noteBackgroundAnimatable = remember {
         Animatable(
-            Color(noteColor)
+            Color(if (noteColor != -1) noteColor else viewModel.noteColor.value)
         )
+    }
+
+    val scope = rememberCoroutineScope()
+
+    LaunchedEffect(key1 = true){
+        viewModel.eventFlow.collectLatest { event ->
+            when(event){
+                is AddEditNoteViewModel.UiEvent.ShowSnackBar -> {
+                    scaffoldState.snackbarHostState.showSnackbar(
+                        message = event.message
+                    )
+
+                }
+
+                is AddEditNoteViewModel.UiEvent.SaveNote -> {
+                    navController.navigateUp()
+                }
+            }
+        }
+    }
+
+    Scaffold(floatingActionButton = {
+
+        FloatingActionButton(
+            onClick = {
+                viewModel.onEvent(AddEditNoteEvent.SaveNote)
+            },
+            backgroundColor = MaterialTheme.colors.primary
+        ) {
+            Icon(imageVector = Icons.Default.Save, contentDescription = "Save Note")
+        }
+
+    }) {
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(noteBackgroundAnimatable.value)
+                .padding(
+                    16.dp
+                )
+        ) {
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+
+            ) {
+
+                Note.noteColors.forEach { color ->
+                    val colorInt = color.toArgb()
+
+                    Box(
+                        modifier = Modifier
+                            .size(50.dp)
+                            .shadow(15.dp, CircleShape)
+                            .clip(CircleShape)
+                            .background(color)
+                            .border(
+                                width = 3.dp,
+                                color = if (viewModel.noteColor.value == colorInt) {
+                                    Color.Black
+                                } else {
+                                    Color.Transparent
+                                }
+                            )
+                            .clickable {
+                                scope.launch {
+                                    noteBackgroundAnimatable.animateTo(
+                                        targetValue = Color(colorInt),
+                                        animationSpec = tween(
+                                            durationMillis = 500
+                                        )
+                                    )
+                                }
+
+                                viewModel.onEvent(AddEditNoteEvent.ChangeColor(colorInt))
+                            }
+                    )
+                }
+
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            TransparentHintTextField(
+                text = titleState.text,
+                hint = titleState.hint,
+                onValueChange = {
+                    viewModel.onEvent(AddEditNoteEvent.EnteredEventTitle(it))
+                },
+                onFocusChange = {
+                    viewModel.onEvent(AddEditNoteEvent.ChangeTitleFocus(it))
+                },
+                isHintVisible = titleState.isHintVisible,
+                singleLine = true,
+                textStyle = MaterialTheme.typography.h5
+            )
+
+
+            TransparentHintTextField(
+                text = contentState.text,
+                hint = contentState.hint,
+                onValueChange = {
+                    viewModel.onEvent(AddEditNoteEvent.EnteredEventContent(it))
+                },
+                onFocusChange = {
+                    viewModel.onEvent(AddEditNoteEvent.ChangeContentFocus(it))
+                },
+                isHintVisible = contentState.isHintVisible,
+                textStyle = MaterialTheme.typography.body1,
+                modifier = Modifier.fillMaxHeight()
+            )
+
+        }
+
     }
 
 }
