96cbbcd0ea2e163359b863533bef44b57d78ee94
Tekkiller <paulnjoroge789016@gmail.com>
2021-10-11 10:36:37 +0300

    Adding add edit note screen

--- app/src/main/java/com/paul/cleanarchnotes/feature_note/presentation/add_edit_note/components/components/AddEditNoteScreen.kt
+++ app/src/main/java/com/paul/cleanarchnotes/feature_note/presentation/add_edit_note/components/components/AddEditNoteScreen.kt
@@ -1,2 +1,31 @@
 package com.paul.cleanarchnotes.feature_note.presentation.add_edit_note.components.components
 
+import androidx.compose.animation.Animatable
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.paul.cleanarchnotes.feature_note.presentation.add_edit_note.components.AddEditNoteViewModel
+
+@Composable
+fun AddEditNoteScreen(
+    navController: NavController,
+    noteColor: Int,
+    viewModel: AddEditNoteViewModel = hiltViewModel()
+) {
+
+    val titleState = viewModel.noteTitle.value
+    val contentState = viewModel.noteContent.value
+    val scaffoldState = rememberScaffoldState()
+
+    val noteBackgroundAnimatable = remember {
+        Animatable(
+            Color(noteColor)
+        )
+    }
+
+}
