8cb33332880c2f4f6658c9632a59335409c71d3f
Tekkiller <paulnjoroge789016@gmail.com>
2021-10-11 11:14:49 +0300

    MainScreen done.

--- app/src/main/java/com/paul/cleanarchnotes/MainActivity.kt
+++ app/src/main/java/com/paul/cleanarchnotes/MainActivity.kt
@@ -3,19 +3,67 @@ package com.paul.cleanarchnotes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.paul.cleanarchnotes.feature_note.presentation.add_edit_note.components.components.AddEditNoteScreen
+import com.paul.cleanarchnotes.feature_note.presentation.notes.components.NotesScreen
+import com.paul.cleanarchnotes.feature_note.presentation.util.Screen
 import com.paul.cleanarchnotes.ui.theme.CleanArchNotesTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CleanArchNotesTheme {
 
+                androidx.compose.material.Surface(color = MaterialTheme.colors.background) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.NoteScreen.route
+                    ){
+                        composable(route = Screen.NoteScreen.route){
+                            NotesScreen(navController = navController)
+                        }
+
+                        composable(route = Screen.NoteScreen.route
+                        + "?noteId={noteId}&noteColor={noteColor}",
+                            arguments = listOf(
+                                navArgument(
+                                    name = "noteId"
+                                ){
+                                    type = NavType.IntType
+                                    defaultValue = -1
+                                },
+                                navArgument(
+                                    name = "noteColor"
+                                ){
+                                    type = NavType.IntType
+                                    defaultValue = -1
+                                }
+
+                            )
+                        ){
+                            val color = it.arguments?.getInt("noteColor") ?: -1
+                            AddEditNoteScreen(navController = navController,
+                                noteColor = color)
+
+                        }
+                    }
+                }
             }
         }
     }
